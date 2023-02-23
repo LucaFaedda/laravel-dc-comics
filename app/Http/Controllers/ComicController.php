e95219633a4c7e0ea2b7d6f4ac5e0a2f0856e966
LucaFaedda <lucakarl91@gmail.com>
2023-02-23 14:52:00 +0100

    first commit

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comics;
+
+class ComicController extends Controller
+{
+    public function index(){
+
+        $listaFooter = config('footer'); 
+        $listaIcone = config('icons');
+        $listaSocial= config('social');
+        $prodotto = Comics::all();
+        return view('home', compact('prodotto','listaFooter', 'listaIcone', 'listaSocial'));
+    }
+    
+    public function show($id){
+        $listaFooter = config('footer'); 
+        $listaIcone = config('icons');
+        $listaSocial= config('social');
+        
+        $fumetto = Comics::find($id);
+        
+        $singolo = [
+            'singolo' => $fumetto
+        ];
+
+        
+        return view('comics_singolo', $singolo,  compact('listaFooter', 'listaIcone', 'listaSocial'));
+        
+    }
+    
+
+}
