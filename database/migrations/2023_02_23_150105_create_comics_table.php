d1aa88def14557ece13283af1b70cebe182734b2
LucaFaedda <lucakarl91@gmail.com>
2023-02-23 16:02:01 +0100

    creato seeder

--- database/migrations/2023_02_23_150105_create_comics_table.php
+++ database/migrations/2023_02_23_150105_create_comics_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 20);
+            $table->string('title', 50);
             $table->text('description');
-            $table->string('thumb', 30);
+            $table->text('thumb');
             $table->float('price', 3,1 );
-            $table->string('series', 20);
+            $table->string('series', 50);
             $table->date('sale_date');
-            $table->string('type', 20);
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comics;
 
 class ComicsSeeder extends Seeder
 {
@@ -15,5 +16,20 @@ class ComicsSeeder extends Seeder
     public function run()
     {
         $comics = config('comics');
+
+        foreach($comics as $comic){
+
+            $newComics = new Comics();
+            $newComics->title = $comic['title'];
+            $newComics->description = $comic['description'];
+            $newComics->thumb = $comic['thumb'];
+            $newComics->price = $comic['price'];
+            $newComics->series = $comic['series'];
+            $newComics->sale_date = $comic['sale_date'];
+            $newComics->type = $comic['type'];
+            $newComics->save();
+
+
+        }
     }
 }
