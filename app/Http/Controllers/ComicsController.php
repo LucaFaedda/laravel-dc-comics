00af18fa0650a69fdbce40a0b8c7594ddf7aa7a3
LucaFaedda <lucakarl91@gmail.com>
2023-02-24 15:06:16 +0100

    collegato al model

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -47,13 +47,14 @@ class ComicsController extends Controller
         $form_data = $request->all();
 
         $newComic = new Comics();
-        $newComic->title = $form_data['title'];
-        $newComic->description = $form_data['description'];
-        $newComic->thumb = $form_data['thumb'];
-        $newComic->price = $form_data['price'];
-        $newComic->series = $form_data['series'];
-        $newComic->sale_date = $form_data['sale_date'];
-        $newComic->type = $form_data['type'];
+        $newComic->fill($form_data);
+        // $newComic->title = $form_data['title'];
+        // $newComic->description = $form_data['description'];
+        // $newComic->thumb = $form_data['thumb'];
+        // $newComic->price = $form_data['price'];
+        // $newComic->series = $form_data['series'];
+        // $newComic->sale_date = $form_data['sale_date'];
+        // $newComic->type = $form_data['type'];
         $newComic->save();
 
         return redirect()->route('comics.show' , $newComic['id']);
--- app/Models/Comics.php
+++ app/Models/Comics.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Comics extends Model //era scritto male comics
 {
     use HasFactory;
+
+    protected $fillable = ['title','description','thumb','price', 'series', 'sale_date', 'type'];
 }
